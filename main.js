1363a7cb647799da40a02744ba6d517e388fd1d4
Danilo M Souza <47028834+danilosoftwares@users.noreply.github.com>
2021-06-13 21:42:25 -0300

    removido menu basico

--- main.js
+++ main.js
@@ -4,5 +4,7 @@ let mainWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow();
+  mainWindow.setMenuBarVisibility(false)
+
   mainWindow.loadURL(path.join('file://', __dirname, 'public/index.html'));
 });
